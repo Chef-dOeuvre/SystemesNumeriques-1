46a8f2c40e745253a3bb5e6b617794e30b9de266
Justin <justin.robert@querbes.net>
2023-04-03 09:55:53 +0200

    Update competences.js

--- js/competences.js
+++ js/competences.js
@@ -1,21 +1,36 @@
 window.onload = function() {
 
-    // Lorsque l'utilisateur fait défiler la page, montrer ou masquer le bouton
-    window.onscroll = function() {
-      const topBtn = document.getElementById("top-btn");
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          topBtn.style.display = "block";
-      } else {
-          topBtn.style.display = "none";
-      }
-    };
+  // Lorsque l'utilisateur fait défiler la page, montrer ou masquer le bouton
+  window.onscroll = function() {
+    const topBtn = document.getElementById("top-btn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+  };
+  
+  // Lorsque l'utilisateur clique sur le bouton, remonter en haut de la page
+  document.getElementById("top-btn").addEventListener("click", function() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });  
+  });
+  };
+  
+  window.addEventListener('DOMContentLoaded', function(){
+    let toggle = document.querySelector('.toggle');
+    let body = document.querySelector('body');
     
-    // Lorsque l'utilisateur clique sur le bouton, remonter en haut de la page
-    document.getElementById("top-btn").addEventListener("click", function() {
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-      });  
+    console.log(toggle);
+    console.log(body);
+    
+    if (toggle && body) {
+    toggle.addEventListener('click', function(){
+    console.log('Toggle clicked');
+    body.classList.toggle('open');
     });
-    };
+    }
+    });
